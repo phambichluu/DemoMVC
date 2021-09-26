08adb1823b6aaaab79f386b38ec6b9a232f48de1
DELL <DELL@DESKTOP-U4OBT9D>
2021-09-26 11:48:38 +0700

    first commit

---
+++ DemoMVC/Data/MvcMovieContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DemoMVC.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Student> Student{ get; set; }
+
+    }
+}
